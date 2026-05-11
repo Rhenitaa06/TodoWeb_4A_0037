794f35393b31eb895c2ae64d835d456047048715
theresia25grace-cloud <theresia25grace@gmail.com>
2026-05-11 11:55:42 +0700

    logika button hapus

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
+const inputTugas   = document.getElementById("inputTugas");
+const inputTanggal = document.getElementById("inputTanggal");
+const btntambah    = document.getElementById("btnTambah");
+const daftarTugas  = document.getElementById("daftarTugas");
 
-const inputTugas = document.getElementById("inputTugas");
-const inputTanggal = document.getElementById("inputTanggal")
-const btntambah = document.getElementById("btnTambah");
-const daftarTugas = document.getElementById("daftarTugas");
-
-btntambah.addEventListener("click",function() {
-
+btntambah.addEventListener("click", function() {
     let teksTugas = inputTugas.value;
-    let tanggal = inputTanggal.value;
+    let tanggal   = inputTanggal.value;
 
-    if(teksTugas === ""){
+    if (teksTugas === "") {
         alert("Data harus dimasukkan!");
         return;
     }
 
-    let listbaru = document.createElement("li");
-    let spanbaru = document.createElement("span");
+    let listbaru    = document.createElement("li");
+    let spanbaru    = document.createElement("span");
     let spanTanggal = document.createElement("span");
+    let btnHapus = document.createElement("button"); 
 
-    spanbaru.innerHTML = teksTugas;
-    spanTanggal.innerHTML = tanggal;
+    spanbaru.innerHTML    = teksTugas;
+    spanTanggal.innerHTML = tanggal ? " : " + tanggal : "";
 
-    listbaru.appendChild(spanbaru);
+    btnHapus.innerHTML = "Hapus"; 
+    btnHapus.classList.add("hapus");
+    btnHapus.addEventListener("click", function() {
+        let konfir = confirm("Yakin ingin menghapus tugas ini?");
+        if (konfir) {
+            daftarTugas.removeChild(listbaru);
+        }
+    });
+
+    let divInfo = document.createElement("div");
+    divInfo.appendChild(spanbaru);
+    divInfo.appendChild(spanTanggal);
+
+    listbaru.appendChild(divInfo);
+    listbaru.appendChild(btnHapus);
     daftarTugas.appendChild(listbaru);
-    listbaru.appendChild(spanTanggal);
 
     const warnabaru = document.querySelectorAll("li");
     warnabaru.forEach((item, index) => {
-        if(index % 2 === 0) {
+        if (index % 2 === 0) {
             item.style.color = "red";
         } else {
             item.style.color = "green";
         }
     });
 
-    inputTugas.Value = "";
-    inputTanggal.Value = "";
+    inputTugas.value   = ""; 
+    inputTanggal.value = ""; 
 });
